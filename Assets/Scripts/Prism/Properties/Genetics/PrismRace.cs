65f43096bcce9f5a11171e1dc7ff9003a1427961
WestiferRobin <w.webb0919@gmail.com>
2023-10-16 19:53:13 -0700

    working on requirments as portfolio, game, and ui updates

--- Assets/Scripts/Prism/Properties/Genetics/PrismRace.cs
+++ Assets/Scripts/Prism/Properties/Genetics/PrismRace.cs
@@ -5,20 +5,20 @@ public enum PrismRace
     Unknown = -1,
 
     // Solar Federation
-    Terrian = 0xFFF, // Terrians
+    Terrian = 0xFFF, // Terrians => Mu
     //Lambda = 0xFF0, // Sataurians
     //Gamma = 0x0F0, // Martians
     //Beta = 0x00F, // Neptonians
     //Epsilon = 0xF08, // Ventonians
 
     // Galatic Empire
-    //Omega = 0x000, // Diablons
+    //Omega = 0x000, // Etherals
     //Alpha = 0xF00, // Internals
-    Zetan = 0xF80, // Zetans
+    Zetan = 0x80F, // Zetans => Psi
     //Theta = 0x0FF, // Collective
     //Phi = 0xF0F, // Mutons
 
     // Seperatists
     //Delta = 0x888, // Exchange
-    //Psi = 0x80F, // Rebellion
+    //Sigma = 0xF80, // Rebellion
 }
